215a54342e8826da128e58d72839e6661788b165
dspedroborges <xpedrostewart@gmail.com>
2026-01-19 15:15:13 -0300

    chore: initial commit

---
+++ lua/custom/plugins/oil.lua
@@ -0,0 +1,14 @@
+return {
+	"stevearc/oil.nvim",
+	dependencies = { "nvim-mini/mini.icons" },
+	config = function()
+		require("mini.icons").setup()
+
+		require("oil").setup({
+			view_options = {
+				show_hidden = true,
+				natural_order = "fast",
+			},
+		})
+	end,
+}
