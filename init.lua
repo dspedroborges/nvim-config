f4108a6e189f43bc48970d1acadafcf7b107e170
dspedroborges <xpedrostewart@gmail.com>
2026-01-22 18:26:00 -0300

    chore: update config

--- init.lua
+++ init.lua
@@ -204,6 +204,9 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
+-- Neotree
+vim.keymap.set("n", "<C-b>", "<Cmd>Neotree toggle<CR>")
+
 -- dd: delete line without yanking
 vim.keymap.set("n", "dd", '"_dd')
 
